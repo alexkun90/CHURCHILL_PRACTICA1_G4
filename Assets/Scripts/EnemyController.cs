97fc1b8949b170fb6bb2e6a55d74372662bfd947
Saul Hernandez <saulhdez28@gmail.com>
2023-10-29 19:42:50 -0600

    Modificacion codigo
    Se agrega Singleton, LevelManager, StateManager y funciones para el contador de enemigos spawneados y enemigos eliminados.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    [SerializeField]
+    int _countTankKill = 0;
+    [SerializeField]
+    TextMeshProUGUI tanksKilled;
+
+    void Awake()
+    {
+        tanksKilled.text = _countTankKill.ToString();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Bullet")
         {
             Destroy(other.gameObject);
             Destroy(gameObject);
+            _countTankKill++;
+            tanksKilled.text = _countTankKill.ToString();
+            StateManager.Instance.setTanksKilled(_countTankKill);
         }
         else if (other.gameObject.tag == "Player")
         {
             Destroy(other.gameObject); //esto es para destruir el el player
+            //LevelManager.Instance.NextScene();
         }
     }
 }
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : Singleton<StateManager>
+{
+    string _name;
+    int _tanksKilled;
+    int _tanksSpawned;
+
+
+    public string getName()
+    {
+        return _name;
+    }
+
+    public void setName(string newName)
+    {
+        _name = newName;
+    }
+
+    public int getTanksKilled()
+    {
+        return _tanksKilled;
+    }
+
+    public void setTanksKilled(int newTanksKilled)
+    {
+        _tanksKilled = newTanksKilled;
+    }
+    public int getTanksSpawned()
+    {
+        return _tanksSpawned;
+    }
+
+    public void setTanksSpawned(int newTanksSpawn)
+    {
+        _tanksSpawned = newTanksSpawn;
+    }
+}
